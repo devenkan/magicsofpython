0baa0c2794fdbe4f699cc79903ad3384c248b7cb
deven kan <deven.kan90@gmail.com>
2019-11-28 09:04:24 +1100

    Update map.py

--- map.py
+++ map.py
@@ -7,18 +7,24 @@ list_numbers=map(lambda x:x*10,list_number)
 #it is iterator so we need to iterate it
 for num in list_numbers:
 	print(num)
+"""
+output
+100
+200
+300
+400
+500
+"""
 
 
 #another example
-#if list keyword is already used it shows error
 
-# list = [1, 2, 3]    # remove this list, then it will work.
 
-# def powerof(num):
-#     return num**2
+def powerof(num):
+    return num**2
 
-# number = [1,2,3,4,5,6,7,8]
+number = [1,2,3,4,5,6,7,8]
 
-# s = list(map( powerof , number))
-# print(s)
-# #Output :- [1, 4, 9, 16, 25, 36, 49, 64]
+s = list(map( powerof , number))
+print(s)
+#Output :- [1, 4, 9, 16, 25, 36, 49, 64]
